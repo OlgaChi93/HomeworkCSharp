5877b40657e27b6f68dc34da1ef94ef246b0c65d
Olga <o.iginova@yandex.com>
2022-08-16 19:10:22 +0300

    task 29 вывод массива заданного через консоль

--- Homework4/Task29/Program.cs
+++ Homework4/Task29/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из
+//  8 элементов и выводит их на экран. 
+// Вводим массив через консоль
+
+int[] a = new int[8];
+for (int i = 0; i < a.Length; i++)
+{
+    Console.WriteLine("Введите {0}-й элемент", i + 1);
+    a[i] = Convert.ToInt32(Console.ReadLine());
+
+}
+
+int count = a.Length;
+int position = 0;
+while (position < count)
+{
+    Console.Write(a[position] + " ");
+    position++;
+}
