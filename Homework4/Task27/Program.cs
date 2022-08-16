2ca2486ac5bb3142369dd067988d9f54582cfb07
Olga <o.iginova@yandex.com>
2022-08-16 17:57:38 +0300

    task 27 сумма цифр у введенного числал

--- Homework4/Task27/Program.cs
+++ Homework4/Task27/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+int number= Convert.ToInt32(Console.ReadLine());
+int result = 0;
+    while (number> 0)
+    {
+        result = result + number% 10;
+        number= number/10 ;
+    }
+ Console.WriteLine($"Сумма цифр будет равна {result}");
