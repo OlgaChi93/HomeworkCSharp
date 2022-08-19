1606939cb048ca4f1282cadf5d994df4542dba84
Olga <o.iginova@yandex.com>
2022-08-19 16:41:39 +0300

    task 38 Разница между максимальным и минимальным числом

--- Homework5/Task38/Program.cs
+++ Homework5/Task38/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и 
+// минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+Console.WriteLine("Введите количество элементов массива");
+int[] arr = new int[Convert.ToInt32(Console.ReadLine())];
+int max = 0;
+int min = 100;
+
+Random rand = new Random();
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(1, 100);
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > max) max = arr[i];
+    if (arr[i] < min) min = arr[i];
+}
+Console.WriteLine("Заданный массив:");
+Console.WriteLine(string.Join(", ", arr));
+
+Console.WriteLine($"Максимальное число = {max}");
+Console.WriteLine($"Минимальное число = {min}");
+Console.WriteLine("Разница между максимальным и минимальным числом =" + (max - min));
