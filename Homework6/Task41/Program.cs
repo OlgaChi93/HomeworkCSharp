438350bffde610a9d35f0c89367dc8a17bb279b3
Olga <o.iginova@yandex.com>
2022-08-24 12:09:58 +0300

    task 41 ввод чисел, опр +

---
+++ Homework6/Task41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+//  Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+
+Console.WriteLine("Введите числа через запятую+пробел");
+
+int[] arr= Console.ReadLine()!.Split(", ").Select(int.Parse).ToArray();
+int find = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0) find = find + 1;
+}
+
+Console.WriteLine($"Количество чисел > 0 в массиве - {find}");
