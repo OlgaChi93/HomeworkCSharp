e7fc0eea114e6167fc6287544020a242feabcfe0
Olga <o.iginova@yandex.com>
2022-08-30 10:21:04 +0300

    main

---
+++ Homework7/Task50/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 50. Напишите программу, которая на вход 
+// принимает позиции элемента в двумерном массиве,
+//  и возвращает значение этого элемента или же указание, 
+//  что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 1 7 -> такого числа в массиве нет
+
+int[,] matrix = new int[,]
+{
+    {1,4,7,2},
+    {5,9,2,3},
+    {8,4,2,4},
+};
+
+Console.WriteLine("Введите номер строки элемента:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите номер столбца элемента:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+bool res = false;
+int find = 0;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (i == a && j == b)
+        {
+            res = true;
+            find = matrix[i, j];
+        }
+    }
+}
+
+
+if (res == true) Console.Write($"Число с индексом [{a},{b}] = {find}");
+else Console.Write($"Числа с индексом [{a},{b}] в массиве нет");
---
+++ Homework7/Task52/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] FillMatrix(int rowsCount, int columnsCount, int LeftRange = 0, int RightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(LeftRange, RightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void FindArithmeticColumnMatrix(int[,] matrix)
+{
+    double arif = 0;
+    double sum = 0;
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+        }
+        arif = sum / matrix.GetLength(0);
+        Console.WriteLine($" Среднеарифметическое столбца {j+1}= {arif:F2}");
+        sum = 0;
+    }
+}
+
+
+Console.WriteLine("Введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = FillMatrix(m, n);
+PrintMatrix(matrix);
+FindArithmeticColumnMatrix(matrix);
+
+
