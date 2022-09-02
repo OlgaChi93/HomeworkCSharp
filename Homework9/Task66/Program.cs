9daf8a57cb83ffbbd1a832104cc3997512a8c455
Olga <o.iginova@yandex.com>
2022-09-02 13:06:56 +0300

    Finally!

--- Homework9/Task66/Program.cs
+++ Homework9/Task66/Program.cs
@@ -5,7 +5,7 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-
+Console.WriteLine("Найдём сумму натуральных элементов в промежутке от M до N. ");
 Console.WriteLine("Введите число M ");
 int M = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число N ");
--- Homework9/Task68/Program.cs
+++ Homework9/Task68/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 68: Напишите программу вычисления 
+// функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+// m = 2, n = 3 -> A(m,n) = 9
+
+Console.WriteLine("Выполняем вычисление функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.");
+Console.WriteLine("Введите число m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if ((m > 0) && (n > 0))
+{
+    int result = Akkerman(m, n);
+    Console.WriteLine($"A(m,n) = {result}");
+}
+else
+{
+    Console.WriteLine("Error. Введите неотрицательные числа согласно условиям задачи.");
+}
+
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return Akkerman(m - 1, 1);
+    else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
